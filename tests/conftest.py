f62f63f2a59b786db09b36fde7eb924b11d1850e
Bernardo Fontes <bernardoxhc@gmail.com>
2020-01-20 13:05:40 -0300

    Add custom field generator to conftest to force AppRegistryNotReady exception

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,6 +29,7 @@ def pytest_configure():
         ] + installed_apps
     else:
         raise NotImplementedError("Tests for % are not supported", test_db)
+
     settings.configure(
         DATABASES={"default": {"ENGINE": db_engine, "NAME": db_name}},
         INSTALLED_APPS=installed_apps,
@@ -37,4 +38,9 @@ def pytest_configure():
         MIDDLEWARE=(),
         USE_TZ=os.environ.get("USE_TZ", False),
     )
+
+    from model_bakery import baker
+
+    baker.generators.add("tests.generic.fields.CustomFieldWithGenerator", None)
+
     django.setup()
