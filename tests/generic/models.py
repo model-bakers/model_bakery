163c451ebf881726e1035a46c4ed3d755404127c
Bernardo Fontes <bernardoxhc@gmail.com>
2020-01-27 14:56:20 -0300

    Remove unpleasant field which is not even being used by the tests

--- tests/generic/models.py
+++ tests/generic/models.py
@@ -72,7 +72,6 @@ class Person(models.Model):
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     happy = models.BooleanField(default=True)
     unhappy = models.BooleanField(default=False)
-    bipolar = models.BooleanField(default=False)
     name = models.CharField(max_length=30)
     nickname = models.SlugField(max_length=36)
     age = models.IntegerField()
