24c25dc9b8d6d6d48f07d4f92dcb60fc519541f4
Michael K <michael-k@users.noreply.github.com>
2020-08-05 08:57:39 +0000

    Add trove classifier for Python 3.8 (#95)
    Tested since PR model-bakers/model_bakery#48

--- setup.py
+++ setup.py
@@ -29,5 +29,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 )
