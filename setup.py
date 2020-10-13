ed7f807d365d31f6973babd3cc3433aa23ba678e
Rust Saiargaliev <rustem.saiargaliev@thermondo.de>
2020-10-13 17:32:50 +0200

    Drop retired Python 3.5
    Python 3.5 did reach end-of-life in September 2020:
    https://www.python.org/downloads/release/python-3510/

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ setuptools.setup(
         "Topic :: Software Development",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
