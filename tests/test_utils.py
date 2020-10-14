70260061742058000e4924d27e34797420f4bf5d
Tim Klein <tim.klein@greenphire.com>
2020-10-14 13:52:43 -0400

    [52] Add unit tests for get_calling_module

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,7 @@
 import pytest
+from inspect import getmodule
 
-from model_bakery.utils import import_from_str
+from model_bakery.utils import import_from_str, get_calling_module
 from tests.generic.models import User
 
 
@@ -13,3 +14,36 @@ def test_import_from_str():
 
     assert import_from_str("tests.generic.models.User") == User
     assert import_from_str(User) == User
+
+
+def test_get_calling_module():
+    # Reference to this very module
+    this_module = getmodule(test_get_calling_module)
+
+    # Once removed is the `pytest` module calling this function
+    pytest_module = get_calling_module(1)
+    assert pytest_module != this_module
+    assert "pytest" in pytest_module.__name__
+
+    # Test functions
+    def dummy_secondary_method():
+        return get_calling_module(2), get_calling_module(3)
+
+    def dummy_method():
+        return (
+            *dummy_secondary_method(),
+            get_calling_module(1),
+            get_calling_module(2)
+        )
+
+    # Unpack results from the function chain
+    sec_mod, sec_pytest_mod, dummy_mod, pytest_mod = dummy_method()
+
+    assert sec_mod == this_module
+    assert "pytest" in sec_pytest_mod.__name__
+    assert dummy_mod == this_module
+    assert "pytest" in pytest_mod.__name__
+
+    # Raise an `IndexError` when attempting to access too many frames removed
+    with pytest.raises(IndexError):
+        assert get_calling_module(100)
