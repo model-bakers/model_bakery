7aef9089cc6384db8957332fb140589af7673631
Bernardo Fontes <bernardoxhc@gmail.com>
2021-01-11 08:32:54 -0300

    Update tests/test_recipes.py

--- tests/test_recipes.py
+++ tests/test_recipes.py
@@ -44,8 +44,8 @@ def test_import_seq_from_recipe():
 def test_import_recipes():
     """Test imports works both for full import paths and for
     `app_name.recipe_name` strings."""
-    assert baker.prepare_recipe("generic.Dog"), baker.prepare_recipe(
-        "tests.generic.Dog"
+    assert baker.prepare_recipe("generic.dog"), baker.prepare_recipe(
+        "tests.generic.dog"
     )
 
 
