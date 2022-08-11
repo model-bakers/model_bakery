5d18c03b8a9139d5d75a3a927a527b8a3fdcdddb
Jair Henrique <jair.henrique@gmail.com>
2022-08-11 09:37:03 -0300

    fix future deprecate warning of django.utils.timezone.utc

--- model_bakery/timezone.py
+++ model_bakery/timezone.py
@@ -1,14 +1,13 @@
 """Utility functions to manage timezone code."""
 
-from datetime import datetime
+from datetime import datetime, timezone
 
 from django.conf import settings
-from django.utils.timezone import utc
 
 
 def tz_aware(value: datetime) -> datetime:
     """Return an UTC-aware datetime in case of USE_TZ=True."""
     if settings.USE_TZ:
-        value = value.replace(tzinfo=utc)
+        value = value.replace(tzinfo=timezone.utc)
 
     return value
