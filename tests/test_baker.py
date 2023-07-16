653f163884289e6495ad16efcce681eead4cac37
nefrob <nefrob.skyguy@gmail.com>
2023-07-16 13:36:28 -0600

    :recycle: Switch to fixture for seed reset

--- tests/test_baker.py
+++ tests/test_baker.py
@@ -1051,17 +1051,19 @@ class TestCreateM2MWhenBulkCreate(TestCase):
         assert list(c1.students.all()) == list(c2.students.all()) == [person]
 
 
-class TestBakerSeeded(TestCase):
-    @pytest.mark.django_db
-    def test_seed(self):
+class TestBakerSeeded:
+    @pytest.fixture()
+    def reset_seed(self):
         old_state = random_gen.baker_random.getstate()
+        yield
+        random_gen.baker_random.setstate(old_state)
+        baker.Baker._global_seed = baker.Baker.SENTINEL
 
+    @pytest.mark.django_db
+    def test_seed(self, reset_seed):
         baker.seed(1)
         assert baker.Baker._global_seed == 1
         assert random_gen.gen_integer() == 55195912693
-
-        random_gen.baker_random.setstate(old_state)
-        baker.Baker._global_seed = baker.Baker.SENTINEL
 
     @pytest.mark.django_db
     def test_unseeded(self):
