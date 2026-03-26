71a02cb11321daa5001730651d8c03408e6e0cff
Benjamin Aduo <56738123+benaduo@users.noreply.github.com>
2026-03-26 11:20:06 +0100

    Update tests/test_baker.py
    I actually like this. Yes lets do this
    
    Co-authored-by: Rust Saiargaliev <hey@amureki.me>

--- tests/test_baker.py
+++ tests/test_baker.py
@@ -1288,8 +1288,8 @@ class TestCreateM2MWhenBulkCreate(TestCase):
         dog = baker.make(models.Dog)
         baker.make(models.HomeOwner, home__dogs=[dog], _quantity=10, _bulk_create=True)
 
-        h1, h2 = models.HomeOwner.objects.all()[:2]
-        assert list(h1.home.dogs.all()) == list(h2.home.dogs.all()) == [dog]
+      for owner in models.HomeOwner.objects.all():
+          assert list(owner.home.dogs.all()) == [dog]
 
 
 class TestBakerSeeded:
